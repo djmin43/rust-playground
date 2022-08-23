8a033c4087388f112051edb80a11a47c3b10c35e
Jay Min <dj.min43@gmail.com>
2022-08-24 07:56:42 +0900

    add mut to variable

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let x = 5;
+    let mut x = 5;
     println!("the value of x is: {x}");
     x = 6;
     println!("the value of x is: {x}");
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/Users/mindongjoon/Desktop/personal/rust/variables/target/debug/variables: /Users/mindongjoon/Desktop/personal/rust/variables/src/main.rs

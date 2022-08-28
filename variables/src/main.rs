609fcea98a9a406179c95841ba4cbc93e7c5d3ad
Jay Min <dj.min43@gmail.com>
2022-08-29 07:15:59 +0900

    print const

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,12 @@
 fn main() {
+
     let mut x = 5;
     println!("the value of x is: {x}");
     x = 6;
     println!("the value of x is: {x}");
+
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+
+    println!("{THREE_HOURS_IN_SECONDS}")
 }
+

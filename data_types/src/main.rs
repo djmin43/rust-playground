4452a0d7feb3e86ca7dc637d83e13d6d7ba34a4e
Jay Min <dj.min43@gmail.com>
2022-08-31 07:08:28 +0900

    trying out numbers

--- data_types/src/main.rs
+++ data_types/src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+//     Scalar(Primitive?) and Compound(Reference)
+//     Scalar: integer, floating-point numbers, booleans, characters
+
+    // let x= 1.0;
+    // let x = x + 9.5;
+    // let y: f32 = 3.0;
+    //
+    // let sum = 5 + 10;
+    //
+    // let difference = 95.5 - 4.3;
+
+    let floored = 2.0/3.0;
+    println!("the value is {}", floored)
+
+
 }
---
+++ data_types/target/debug/data_types.d
@@ -0,0 +1,1 @@
+/Users/mindongjoon/Desktop/personal/rust/data_types/target/debug/data_types: /Users/mindongjoon/Desktop/personal/rust/data_types/src/main.rs

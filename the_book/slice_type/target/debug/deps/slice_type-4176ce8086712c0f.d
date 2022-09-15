213fb422f7e29f2ca0450fcc206ac932e588715d
jay <dj.min43@gmail.com>
2022-09-15 18:45:30 +0900

    chore

---
+++ the_book/slice_type/target/debug/deps/slice_type-4176ce8086712c0f.d
@@ -0,0 +1,5 @@
+/Users/dongjoonmin/Desktop/dev/rust-playground/the_book/slice_type/target/debug/deps/slice_type-4176ce8086712c0f: src/main.rs
+
+/Users/dongjoonmin/Desktop/dev/rust-playground/the_book/slice_type/target/debug/deps/slice_type-4176ce8086712c0f.d: src/main.rs
+
+src/main.rs:
--- the_book/slice_type/target/debug/slice_type.d
+++ the_book/slice_type/target/debug/slice_type.d
@@ -1,1 +1,1 @@
-/Users/mindongjoon/Developer/personal/rust/the_book/slice_type/target/debug/slice_type: /Users/mindongjoon/Developer/personal/rust/the_book/slice_type/src/main.rs
+/Users/dongjoonmin/Desktop/dev/rust-playground/the_book/slice_type/target/debug/slice_type: /Users/dongjoonmin/Desktop/dev/rust-playground/the_book/slice_type/src/main.rs

926a2795c2c1d9e2ac7a77f3a511d405ee3a7717
Jay Min <dj.min43@gmail.com>
2022-09-21 07:06:50 +0900

    using other value

--- the_book/struct-data/src/main.rs
+++ the_book/struct-data/src/main.rs
@@ -1,4 +1,19 @@
 fn main() {
+    let user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1,
+    };
+
+    let user2 = User {
+        active: user1.active,
+        username: user1.username,
+        email: String::from("another@example.coma"),
+        sign_in_count: user1.sign_in_count
+    };
+
+
 
 }
 
@@ -7,4 +22,13 @@ struct User {
     username: String,
     email: String,
     sign_in_count: u64
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        email,
+        username,
+        active: true,
+        sign_in_count: 1,
+    }
 }

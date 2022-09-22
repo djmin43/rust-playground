c2856c236257a5e787462939d794814a1eba910e
jay <dj.min43@gmail.com>
2022-09-22 20:18:07 +0900

    strcut

--- the_book/struct-data/src/main.rs
+++ the_book/struct-data/src/main.rs
@@ -10,18 +10,33 @@ fn main() {
         active: user1.active,
         username: user1.username,
         email: String::from("another@example.coma"),
-        sign_in_count: user1.sign_in_count
+        sign_in_count: user1.sign_in_count,
     };
 
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
 
+    let black_point = &black.0;
+    let origin_point = origin.2;
+
+    println!("{}", black_point);
+    println!("{}", origin.2);
+
+    let x = "hello";
+    let c = String::from("bye");
+    let y = x;
+    println!("{}", x)
 
 }
+
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
 
 struct User {
     active: bool,
     username: String,
     email: String,
-    sign_in_count: u64
+    sign_in_count: u64,
 }
 
 fn build_user(email: String, username: String) -> User {
---
+++ the_book/struct-data/target/debug/struct-data.d
@@ -0,0 +1,1 @@
+/Users/dongjoonmin/Desktop/dev/rust-playground/the_book/struct-data/target/debug/struct-data: /Users/dongjoonmin/Desktop/dev/rust-playground/the_book/struct-data/src/main.rs

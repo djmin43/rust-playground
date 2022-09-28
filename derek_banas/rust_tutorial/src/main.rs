d2c7face4cf28549e01aefbce64cb66c9795ddd3
jay <dj.min43@gmail.com>
2022-09-28 20:15:48 +0900

    add

--- derek_banas/rust_tutorial/src/main.rs
+++ derek_banas/rust_tutorial/src/main.rs
@@ -10,7 +10,14 @@ fn main() {
     let greeting: &str = "Nice to meet you";
     io::stdin().read_line(&mut name)
         .expect("Didn't Receive Input");
-
     println!("Hello {}! {}", name.trim_end(), greeting);
 
+    const ONE_MIL: u32 = 1_000_000;
+    const PI: f32 = 3.141592;
+    let age = "47";
+    let mut age: u32 = age.trim().parse()
+        .expect("Age wasn't assigned a number");
+
+    age = age + 1;
+    println!("I am {} and I want ${}", age, ONE_MIL);
 }

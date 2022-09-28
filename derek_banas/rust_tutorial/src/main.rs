43ddaf38f9f9cd7e0a2c7418814e52a3edd0bcd3
jay <dj.min43@gmail.com>
2022-09-29 08:20:12 +0900

    rust

--- derek_banas/rust_tutorial/src/main.rs
+++ derek_banas/rust_tutorial/src/main.rs
@@ -5,19 +5,35 @@ use std::fs::File;
 use std::cmp::Ordering;
 
 fn main() {
-    println!("What is your name?");
-    let mut name = String::new();
-    let greeting: &str = "Nice to meet you";
-    io::stdin().read_line(&mut name)
-        .expect("Didn't Receive Input");
-    println!("Hello {}! {}", name.trim_end(), greeting);
+    // println!("What is your name?");
+    // let mut name = String::new();
+    // let greeting: &str = "Nice to meet you";
+    // io::stdin().read_line(&mut name)
+    //     .expect("Didn't Receive Input");
+    // println!("Hello {}! {}", name.trim_end(), greeting);
+    //
+    // const ONE_MIL: u32 = 1_000_000;
+    // const PI: f32 = 3.141592;
+    // let age = "47";
+    // let mut age: u32 = age.trim().parse()
+    //     .expect("Age wasn't assigned a number");
+    //
+    // age = age + 1;
+    // println!("I am {} and I want ${}", age, ONE_MIL);
 
-    const ONE_MIL: u32 = 1_000_000;
-    const PI: f32 = 3.141592;
-    let age = "47";
-    let mut age: u32 = age.trim().parse()
-        .expect("Age wasn't assigned a number");
+    // println!("Max u32: {}", u32::MAX);
+    // println!("Max u64: {}", u64::MAX);
+    // println!("Max usize: {}", usize::MAX);
+    // println!("Max u128: {}", u128::MAX);
+    // println!("Max f32: {}", f32::MAX);
+    // println!("Max f64: {}", f64::MAX);
 
-    age = age + 1;
-    println!("I am {} and I want ${}", age, ONE_MIL);
+    // let is_true = true;
+    // let my_grade = 'A';
+
+    let num_1: f32 = 1.11111111111111;
+    println!("f32: {}", num_1 + 0.111111111111111);
+
+
+
 }

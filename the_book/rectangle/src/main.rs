fb2eab04b12e465fe07b88dd7497c30c07e15ed1
jay <dj.min43@gmail.com>
2022-09-30 19:45:35 +0900

    with tuple

--- the_book/rectangle/src/main.rs
+++ the_book/rectangle/src/main.rs
@@ -3,9 +3,17 @@ fn main() {
     let height1 = 50;
     let rect_area = area(width1, height1);
     println!("{}", rect_area);
+
+
+    let rect1 = (30,50);
+    println!("{}", area_tuple(rect1))
 }
 
 
 fn area(width: u32, height: u32) -> u32 {
     width * height
+}
+
+fn area_tuple(dimensions: (u32, u32)) ->  u32{
+    dimensions.0 * dimensions.1
 }

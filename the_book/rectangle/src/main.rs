9fcedb6f076aa87b183d175913e13d4970d4c251
jay <dj.min43@gmail.com>
2022-09-30 20:01:24 +0900

    rect struct

--- the_book/rectangle/src/main.rs
+++ the_book/rectangle/src/main.rs
@@ -1,12 +1,24 @@
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
 fn main() {
     let width1 = 30;
     let height1 = 50;
     let rect_area = area(width1, height1);
     println!("{}", rect_area);
 
-
     let rect1 = (30,50);
-    println!("{}", area_tuple(rect1))
+    println!("{}", area_tuple(rect1));
+
+    let rect3 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    let rect3_area = area_struct(&rect3);
+    println!("{}", rect3_area);
 }
 
 
@@ -16,4 +28,8 @@ fn area(width: u32, height: u32) -> u32 {
 
 fn area_tuple(dimensions: (u32, u32)) ->  u32{
     dimensions.0 * dimensions.1
+}
+
+fn area_struct(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }

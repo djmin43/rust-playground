991a947aa5f09e3cd48d439e999295a19c1114bf
jay <dj.min43@gmail.com>
2022-09-30 19:17:56 +0900

    add rect area

--- the_book/rectangle/src/main.rs
+++ the_book/rectangle/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let width1 = 30;
+    let height1 = 50;
+    let rect_area = area(width1, height1);
+    println!("{}", rect_area);
+}
+
+
+fn area(width: u32, height: u32) -> u32 {
+    width * height
 }
---
+++ the_book/rectangle/target/debug/deps/rectangle-6435e67ae863b128.d
@@ -0,0 +1,5 @@
+/Users/dongjoonmin/Desktop/dev/rust-playground/the_book/rectangle/target/debug/deps/rectangle-6435e67ae863b128: src/main.rs
+
+/Users/dongjoonmin/Desktop/dev/rust-playground/the_book/rectangle/target/debug/deps/rectangle-6435e67ae863b128.d: src/main.rs
+
+src/main.rs:

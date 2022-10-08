757a748473da10c310479e53d84421efa79c3d75
jay <dj.min43@gmail.com>
2022-10-08 19:29:31 +0900

    new

--- the_book/define_enum/src/main.rs
+++ the_book/define_enum/src/main.rs
@@ -1,41 +1,7 @@
 
 fn main() {
 
-    let home = IpAddr::V4(String::from("127.0.0.1"));
 
-    let loopback = IpAddr::V6(String::from("::1"));
-
-    let quit_message = Message::Quit;
-
-    let move_message = Message::Move {x: 3, y: 1};
-
-    let write_message = Message::Write(String::from("hello world"));
-
-    write_message.call();
-
-    let change_color_message = Message::ChangeColor(3, 1, 2);
-}
-
-enum IpAddr {
-    V4(String),
-    V6(String)
 }
 
 
-enum IpAddrKind {
-    V4,
-    V6,
-}
-
-enum Message {
-    Quit,
-    Move { x: i32, y: i32 },
-    Write(String),
-    ChangeColor(i32, i32, i32),
-}
-
-impl Message {
-    fn call(&self) {
-        // method body would be defined here
-    }
-}

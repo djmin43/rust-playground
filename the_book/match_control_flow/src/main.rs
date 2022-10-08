a8b564da761b4659944bda3320297b73673eb92c
jay <dj.min43@gmail.com>
2022-10-08 21:54:00 +0900

    value in cents

--- the_book/match_control_flow/src/main.rs
+++ the_book/match_control_flow/src/main.rs
@@ -1,11 +1,16 @@
 fn main() {
     let value = value_in_cents(Coin::Dime);
     println!("{}", value);
+    value_in_cents(Coin::Penny);
+
 }
 
 fn value_in_cents (coin: Coin) -> u8 {
     match coin {
-        Coin::Penny => 1,
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        },
         Coin::Nickel => 5,
         Coin::Dime => 10,
         Coin::Quarter => 25,

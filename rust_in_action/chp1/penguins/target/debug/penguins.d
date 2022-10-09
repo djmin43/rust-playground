a1b70e3f79be06ced2b714881e684d27072870df
jay <dj.min43@gmail.com>
2022-10-09 13:49:04 +0900

    penguins

---
+++ rust_in_action/chp1/penguins/target/debug/penguins.d
@@ -0,0 +1,1 @@
+/Users/dongjoonmin/Desktop/dev/rust-playground/rust_in_action/chp1/penguins/target/debug/penguins: /Users/dongjoonmin/Desktop/dev/rust-playground/rust_in_action/chp1/penguins/src/main.rs

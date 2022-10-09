bbd900e1325b338aba733d87da4d43e0d687b633
jay <dj.min43@gmail.com>
2022-10-09 11:37:38 +0900

    interate

---
+++ rust_in_action/chp1/hello2/target/debug/deps/hello2-8927daffd0c44965.d
@@ -0,0 +1,5 @@
+/Users/dongjoonmin/Desktop/dev/rust-playground/rust_in_action/chp1/hello2/target/debug/deps/hello2-8927daffd0c44965: src/main.rs
+
+/Users/dongjoonmin/Desktop/dev/rust-playground/rust_in_action/chp1/hello2/target/debug/deps/hello2-8927daffd0c44965.d: src/main.rs
+
+src/main.rs:
---
+++ rust_in_action/chp1/hello2/target/debug/hello2.d
@@ -0,0 +1,1 @@
+/Users/dongjoonmin/Desktop/dev/rust-playground/rust_in_action/chp1/hello2/target/debug/hello2: /Users/dongjoonmin/Desktop/dev/rust-playground/rust_in_action/chp1/hello2/src/main.rs

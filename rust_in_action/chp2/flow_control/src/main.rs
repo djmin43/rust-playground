53982422e728d09cc68d624c9a3ae3454ef090db
jay <dj.min43@gmail.com>
2022-10-10 21:46:33 +0900

    ref

---
+++ rust_in_action/chp2/flow_control/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+
+    let a = 42;
+    let r = &a;
+    let b = a + *r;
+
+    println!("a + a = {}", b);
+}
---
+++ rust_in_action/chp2/flow_control/target/debug/deps/flow_control-31ad2b35051daac8.d
@@ -0,0 +1,5 @@
+/Users/dongjoonmin/Desktop/dev/rust-playground/rust_in_action/chp2/flow_control/target/debug/deps/flow_control-31ad2b35051daac8: src/main.rs
+
+/Users/dongjoonmin/Desktop/dev/rust-playground/rust_in_action/chp2/flow_control/target/debug/deps/flow_control-31ad2b35051daac8.d: src/main.rs
+
+src/main.rs:

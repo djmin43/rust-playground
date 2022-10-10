7a6362abaf080c1cb2bb22071630e0ba74e1a802
jay <dj.min43@gmail.com>
2022-10-10 20:48:07 +0900

    number type casting

---
+++ rust_in_action/chp2/numbers/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let a: i32 = 10;
+    let b: u16 = 100;
+
+    let b_ = b.try_into()
+        .unwrap();
+
+    if a < b_ {
+        println!("ten is less than one hundred.");
+    }
+}
---
+++ rust_in_action/chp2/numbers/target/debug/numbers.d
@@ -0,0 +1,1 @@
+/Users/dongjoonmin/Desktop/dev/rust-playground/rust_in_action/chp2/numbers/target/debug/numbers: /Users/dongjoonmin/Desktop/dev/rust-playground/rust_in_action/chp2/numbers/src/main.rs

1faa673c41876d21557886b15d58f4a0ba792a58
jay <dj.min43@gmail.com>
2022-11-12 11:23:59 +0900

    getting the idea of mod

--- the_book/restaurant/src/lib.rs
+++ the_book/restaurant/src/lib.rs
@@ -1,14 +1,50 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
+
+        fn seat_at_table() {}
+    }
+
+    mod serving {
+        fn take_order() {}
+
+        fn serve_order() {}
+
+        fn take_payment() {}
+    }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+use crate::front_of_house::hosting;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+use crate::back_of_house::kitchen;
+
+pub fn eat_at_restaurant() {
+    hosting::add_to_waitlist();
+
+
+    kitchen::washing_dishes();
+
+    // Absolute path
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    // Relative path
+    front_of_house::hosting::add_to_waitlist();
+}
+
+fn deliver_order() {}
+
+mod back_of_house {
+
+    pub mod kitchen {
+        pub fn washing_dishes() {
+
+        }
     }
+
+    fn fix_incorrect_order() {
+        cook_order();
+        super::deliver_order();
+    }
+
+    fn cook_order() {}
 }

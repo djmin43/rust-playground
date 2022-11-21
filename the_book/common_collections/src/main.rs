ed0ea91ecb5441daaebab826780e8bc89648ee22
Jay Min <dj.min43@gmail.com>
2022-11-22 07:27:10 +0900

    compre strings length

--- the_book/common_collections/src/main.rs
+++ the_book/common_collections/src/main.rs
@@ -1,9 +1,13 @@
 
 fn main() {
 
-    let mut s1 = String::from("foo");
-    let s2 = "bar";
-    s1.push_str(s2);
-    println!("s2 is {}", s2);
+    // let s = "hello";
+
+    let s2 = String::from("world");
+
+    // let s1 = s.len();
+    let s1 = s2.len();
+
+    println!("{}", s1);
 
 }

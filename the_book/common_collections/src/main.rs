910685efabbef1d2b68f5b953cf4730d545c741b
Jay Min <dj.min43@gmail.com>
2022-11-24 07:43:55 +0900

    .

--- the_book/common_collections/src/main.rs
+++ the_book/common_collections/src/main.rs
@@ -1,9 +1,10 @@
 use std::collections::HashMap;
 
 fn main() {
-    let mut scores = HashMap::new();
 
-    scores.insert(String::from("Blue"), 10);
-    scores.insert(String::from("Yellow"), 50);
+    let field_name = String::from("Favorite color");
+    let field_value = String::from("Blue");
 
+    let mut map = HashMap::new();
+    map.insert(field_name, field_value);
 }

8400a4d1fba11fb759ee86d6b38ed49cd86849d9
jay <dj.min43@gmail.com>
2022-11-23 21:40:40 +0900

    string

--- the_book/common_collections/src/main.rs
+++ the_book/common_collections/src/main.rs
@@ -1,13 +1,8 @@
 
 fn main() {
 
-    // let s = "hello";
+    let hello = "Здравствуйте";
+    let answer = &hello[0];
 
-    let s2 = String::from("world");
-
-    // let s1 = s.len();
-    let s1 = s2.len();
-
-    println!("{}", s1);
-
+    println!("{}", answer)
 }

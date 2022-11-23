f20fc6d291766d74cec81a93c0f780ede6efc143
jay <dj.min43@gmail.com>
2022-11-23 21:44:09 +0900

    range to get index of string

--- the_book/common_collections/src/main.rs
+++ the_book/common_collections/src/main.rs
@@ -2,7 +2,8 @@
 fn main() {
 
     let hello = "Здравствуйте";
-    let answer = &hello[0];
+
+    let answer = &hello[0..4];
 
     println!("{}", answer)
 }
--- the_book/common_collections/target/debug/common_collections.d
+++ the_book/common_collections/target/debug/common_collections.d
@@ -1,1 +1,1 @@
-/Users/mindongjoon/Developer/personal/rust/the_book/common_collections/target/debug/common_collections: /Users/mindongjoon/Developer/personal/rust/the_book/common_collections/src/main.rs
+/Users/dongjoonmin/Desktop/dev/rust-playground/the_book/common_collections/target/debug/common_collections: /Users/dongjoonmin/Desktop/dev/rust-playground/the_book/common_collections/src/main.rs

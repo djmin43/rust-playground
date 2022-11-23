0e61d295fe624b224169f8cf306574559ba23781
Jay Min <dj.min43@gmail.com>
2022-11-24 07:13:24 +0900

    rust

--- the_book/common_collections/src/main.rs
+++ the_book/common_collections/src/main.rs
@@ -1,6 +1,9 @@
+use std::collections::HashMap;
 
 fn main() {
-    for d in "이우혁".bytes() {
-        println!("{}", d);
-    }
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
 }

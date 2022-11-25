05dc81774c45ccc2d51e25dbd6dfd73a79910c5d
Jay Min <dj.min43@gmail.com>
2022-11-25 23:26:14 +0900

    get value by copying

--- the_book/common_collections/src/main.rs
+++ the_book/common_collections/src/main.rs
@@ -2,9 +2,13 @@ use std::collections::HashMap;
 
 fn main() {
 
-    let field_name = String::from("Favorite color");
-    let field_value = String::from("Blue");
+    let mut scores = HashMap::new();
 
-    let mut map = HashMap::new();
-    map.insert(field_name, field_value);
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    let team_name = String::from("Blue");
+
+    let score = scores.get(&team_name).copied().unwrap_or(0);
+
 }

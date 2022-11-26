ea11d0bbaaa3de93a2a7954f33bbcdfdcdfe6e4b
jay <dj.min43@gmail.com>
2022-11-26 11:10:49 +0900

    or insert yellow

--- the_book/common_collections/src/main.rs
+++ the_book/common_collections/src/main.rs
@@ -8,6 +8,7 @@ fn main() {
     scores.insert(String::from("Blue"), 25);
 
     scores.entry(String::from("Blue")).or_insert(30);
+    scores.entry(String::from("Yellow")).or_insert(30);
 
     println!("{:?}", scores);
 

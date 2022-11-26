ab6b8d2ea341a8a0c5abd163d82c50639a56e95b
jay <dj.min43@gmail.com>
2022-11-26 10:56:54 +0900

    hashmap for loop

--- the_book/common_collections/src/main.rs
+++ the_book/common_collections/src/main.rs
@@ -7,8 +7,9 @@ fn main() {
     scores.insert(String::from("Blue"), 10);
     scores.insert(String::from("Yellow"), 50);
 
-    let team_name = String::from("Blue");
 
-    let score = scores.get(&team_name).copied().unwrap_or(0);
+    for (key, value) in &scores {
+        println!("{}, {}", key, value)
+    }
 
 }

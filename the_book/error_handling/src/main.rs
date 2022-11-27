d1d6fb358c6c62e1f176c573c8e69871f6835cd4
jay <dj.min43@gmail.com>
2022-11-27 14:55:48 +0900

    panic!

--- the_book/error_handling/src/main.rs
+++ the_book/error_handling/src/main.rs
@@ -1,6 +1,11 @@
+use std::fs::File;
+
 fn main() {
-    enum Result<T, E> {
-        Ok(T),
-        Err(E)
-    }
+    let greeting_file_result = File::open("hello.txt");
+
+    let greeting_file = match greeting_file_result {
+        Ok(file) => file,
+        Err(error) => panic!("Problem opening the file: {:?}", error),
+    };
+
 }
